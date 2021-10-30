92a504714557515223912906082ce041ee6245eb
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2021-10-30 09:13:52 -0700

    update template

--- template_files/scripts/template.py
+++ template_files/scripts/template.py
@@ -3,17 +3,14 @@ import fire
 import config  # noqa: F401
 from PyImporter import (
     BaseModel,
-    format_data_file_path,
 )
 
 
-class Demo(BaseModel):
+class Object(BaseModel):
     def __init__(self):
         self.table = "ca_table"
         self.type = "ca_type"
-        # if you only import one file per script, set self.file and self.file_path
         self.file = "demo.csv"
-        self.file_path = format_data_file_path(self.file)
 
     def validation(self, df, metadata):
         pass
@@ -29,6 +26,8 @@ class Demo(BaseModel):
                 "file_type": "csv",
                 "table": self.table,
                 "type": self.type,
+                "idno_file_column": "file_column_name",
+                "idno_format": "slug",
                 "bundles": [{"CollectiveAccess_field": "file_column_name"}],
                 "related_fields": self.related_fields(),
                 "relationships": self.relationships(),
@@ -40,8 +39,13 @@ class Demo(BaseModel):
             {
                 "target_table": "ca_table",
                 "target_type": "ca_table_type",
-                "target_column": "file_column_name",
+                "target_file_column": "file_column_name",
                 "relationship": "relationship_name",
+                "idno_format": "slug",
+                "not_found_action": {
+                    "action": "create_record",
+                    "bundles": [{"CollectiveAccess_field": "file_column_name"}],
+                },
             },
         ]
 
@@ -50,23 +54,30 @@ class Demo(BaseModel):
             {
                 "target_table": "ca_table",
                 "target_type": "ca_table_type",
-                "target_column": "file_column_name",
+                "target_file_column": "file_column_name",
                 "subject_field": "Collective_Access_field",
+                "not_found_action": {
+                    "action": "update_field",
+                    "bundles": [{"CollectiveAccess_field": "file_column_name"}],
+                },
             },
         ]
 
 
 if __name__ == "__main__":
-    obj = Demo()
+    obj = Object()
     fire.Fire(
         {
             "validation": obj.validation_from_metadata,
-            "create_records": obj.create_records_from_metadata,
             "preview_create": obj.preview_create_query_from_metadata,
-            "truncate_table": obj.truncate_table,
-            "delete_records": obj.delete_records,
-            "delete_relationships": obj.delete_relationships_from_metadata,
+            "create_records": obj.create_records_from_metadata,
             "preview_update": obj.preview_update_query_from_metadata,
             "update_records": obj.update_records_from_metadata,
+            "preview_delete": obj.preview_delete_records,
+            "delete_records": obj.delete_records,
+            "preview_delete_relationships": obj.preview_delete_relationships_from_metadata,  # noqa: E501
+            "delete_relationships": obj.delete_relationships_from_metadata,
+            "truncate_list": obj.truncate_list,
+            "truncate_table": obj.truncate_table,
         }
     )
