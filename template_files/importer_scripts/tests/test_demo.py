ecf7d05fdc2e8549da5a977cd1e6f9bf4d6b19d1
Wai-Yin Kwan <wykhuh@users.noreply.github.com>
2022-07-06 19:08:04 -0700

    fix tests

--- template_files/importer_scripts/tests/test_demo.py
+++ template_files/importer_scripts/tests/test_demo.py
@@ -7,19 +7,19 @@ class TestCreateRecordsFromMetadata:
         instance.search_cache = {}
         connect_mock = mocker.patch(
             "scripts.utils.connect_api.execute",
-            return_value={"find": {"count": 0}, "add": {"id": 1}},
+            return_value={"find": {"count": 0}, "add": {"id": [1], "idno": ["idno1"]}},
         )
 
         instance.create_records_from_metadata(1)
 
-        assert connect_mock.call_count == 16
+        assert connect_mock.call_count == 15
 
 
 class TestPreviewCreateQueryFromMetadata:
     def test_prints_data_from_file(self, mocker):
         instance = demo.Demo()
         instance.search_cache = {}
-        print_mock = mocker.patch("builtins.print")
+        print_mock = mocker.patch("scripts.utils.logger.console.warning")
         connect_mock = mocker.patch(
             "scripts.utils.connect_api.execute",
             return_value={"find": {"count": 0}, "add": {"id": 1}},
@@ -28,4 +28,4 @@ class TestPreviewCreateQueryFromMetadata:
         instance.preview_create_query_from_metadata(1)
 
         assert print_mock.call_count == 8
-        assert connect_mock.call_count == 5
+        assert connect_mock.call_count == 6
